d2709e953ae5a474c651392e85bb7ea895a8699d
Guaci Gutierrez <guacig@hotmail.com>
2023-05-02 18:26:34 +0100

    create user and sonner notifications

--- src/components/CreateNewUser.tsx
+++ src/components/CreateNewUser.tsx
@@ -1,19 +1,50 @@
-import { Button, Card, TextInput, Title } from "@tremor/react";
+import { Badge, Button, Card, TextInput, Title } from "@tremor/react";
+import { useState } from "react";
+import { useUserActions } from "../hooks/useUserActions";
 
 export function CreateNewUser() {
+	const { addUser } = useUserActions();
+	const [result, setResult] = useState<"ok" | "ko" | null>(null);
+
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+
+		setResult(null);
+
+		const form = event.target as HTMLFormElement;
+		const formData = new FormData(form);
+
+		const name = formData.get("name") as string;
+		const email = formData.get("email") as string;
+		const github = formData.get("github") as string;
+
+		if (!name || !email || !github) {
+			// any validation
+			return setResult("ko");
+		}
+
+		addUser({ name, email, github });
+		setResult("ok");
+		form.reset();
+	};
+
 	return (
 		<Card style={{ marginTop: "16px" }}>
 			<Title>Create New User</Title>
 
-			<form className=''>
-				<TextInput placeholder='User Name' />
-				<TextInput placeholder='User Email' />
-				<TextInput placeholder='GitHub UserName ' />
+			<form onSubmit={handleSubmit} className=''>
+				<TextInput name="name" placeholder='User Name' />
+				<TextInput name="email" placeholder='User Email' />
+				<TextInput name="github" placeholder='GitHub UserName ' />
 
 				<div>
 					<Button type='submit' style={{ marginTop: "16px" }}>
 						Create User
 					</Button>
+					<span>
+						{result === "ok" && <Badge color="green">Properly stored</Badge>}
+						{result === "ko" && <Badge color="red"> Error on fields </Badge>}
+					</span>
 				</div>
 			</form>
 		</Card>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,49 @@
-import { configureStore } from "@reduxjs/toolkit";
-import usersReducer from "./users/slice";
+import { configureStore, type Middleware } from "@reduxjs/toolkit";
+import { toast } from "sonner";
+import usersReducer, { rollbackUser } from "./users/slice";
 
-const persistanceLocalStorageMiddleware = (store) => (next) => (action) => {
+const persistanceLocalStorageMiddleware: Middleware =
+	(store) => (next) => (action) => {
+		next(action);
+		localStorage.setItem("__redux__state__", JSON.stringify(store.getState()));
+	};
+
+const syncWithDatabase: Middleware = (store) => (next) => (action) => {
+	const { type, payload } = action;
+
+	const previousState = store.getState() as RootState;
 	next(action);
-	localStorage.setItem("__redux__state__", JSON.stringify(store.getState()));
+
+	//
+	if (type === "users/deleteUserById") {
+		const userIdToRemove = payload;
+		const userToRemove = previousState.users.find(
+			(user) => user.id === userIdToRemove,
+		);
+
+		fetch(`https://jsonplaceholder.typicode.com/users/${userIdToRemove}`, {
+			method: "DELETE",
+		})
+			.then((res) => {
+				if (res.ok) {
+					toast.success(`User with ${payload} has been deleted`);
+				}
+				throw new Error("Error deleting user");
+			})
+			.catch((err) => {
+				toast.error(`Error deleting user ${userIdToRemove}`);
+				if (userToRemove) store.dispatch(rollbackUser(userToRemove));
+				console.log(err);
+				console.log("error");
+			});
+	}
 };
 
 export const store = configureStore({
 	reducer: {
 		users: usersReducer,
 	},
-	middleware: [persistanceLocalStorageMiddleware],
+	middleware: [persistanceLocalStorageMiddleware, syncWithDatabase],
 });
 
 export type RootState = ReturnType<typeof store.getState>;
